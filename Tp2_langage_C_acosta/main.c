a768779d50dd725bfdf2bfcaf650e79cfd1af883
BASSINAS <edembassine@gmail.com>
2019-11-12 01:15:18 +0100

    affichage des valeurs calculer dans un tableau dynamique

--- Tp2_langage_C_acosta/main.c
+++ Tp2_langage_C_acosta/main.c
@@ -3,14 +3,13 @@
 
 double f(double x)
         {
-            return (3*x*2+2*x-7); // Cette fonction est un exemple
+            return x*x; // Cette fonction est un exemple
         }
 int main()
         {
-            int i;
-            double a,b,pas,x,y;
-            FILE *fp;
-            fp=fopen("function.txt","w");
+            int i =0;
+            double a,b,pas,x;
+            double *ptr_tableau = NULL;
             printf("a = ");
             scanf("%lf",&a);
             printf("b = ");
@@ -19,15 +18,14 @@ int main()
             scanf("%lf",&pas);
             printf("Vous avez tapé [%f, %f],pas= %f\n",a,b,pas);
             int n=(b-a)/pas;
+            ptr_tableau = malloc(sizeof(double)*n);
             printf("N Echantillons %d\n",n);
             x=a;
             for (i=0;i<n; i++)
                     {
                         x+=pas;
-                        y=f(x);
-                        fprintf(fp,"x = %f,\t y = %f\n",x,y);
-                        printf("%f\t f(%f) = %f\n",x,x,y);
+                        ptr_tableau[i] = f(x);
+                        printf("x = %f\t tab[%d] = %f\n",x,i+1,ptr_tableau[i]);
                     }
-            fclose(fp);
     return 0;
     }
